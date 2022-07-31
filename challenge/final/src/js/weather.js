30589664bed90be3fa9e97034a6519c9f8fe4856
hushkmj <hushkmj@naver.com>
2022-07-31 22:30:01 +0900

    노마드코더 바닐라js 챌린지 Final
    - 실시간 시계
    - 로컬 스토리지를 사용한 로그인
    - 로컬 스토리지를 사용한 투두리스트
    - 랜덤 배경 이미지
    - 날씨와 위치 (geolocation)

---
+++ challenge/final/src/js/weather.js
@@ -0,0 +1,20 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "bdc0d41d904c092a750d26c9e469b039";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
